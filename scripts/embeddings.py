2d6098bc425665fb049caf6853164dd995b64985
andrei.tsypilnikov <andrei.tsypilnikov92@gmail.com>
2020-02-22 12:26:33 +0300

    Fixed embeddings

--- scripts/embeddings.py
+++ scripts/embeddings.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import normalize
 
 from com.expleague.media_space.topics.fast_qt import FastQt
 
-DATA_DIR = "/home/andreitsy/Yandex.Disk/cs_center/topics/data/models"
+DATA_DIR = "/home/tsypia/git/topics/topic_modeling/models/gasparetti"
 
 
 def load_fasttext(fname, skip=0, limit=10e9):
@@ -31,12 +31,14 @@ def load_fasttext(fname, skip=0, limit=10e9):
 
 # noinspection PyArgumentList
 def main():
-    data, words = load_fasttext(os.path.join(DATA_DIR, "news_dragnet.vec"), 1000, 90000)
+    data, words = load_fasttext(os.path.join(DATA_DIR, "news_dragnet.vec"), 2000, 100000)
     # data, words = load_glove('lenta-decomp-new', 20000)
 
     data = normalize(data)
-    threshold = 0.75
-    min_cluster = 60
+    #threshold = 0.8
+    #min_cluster = 50
+    threshold = 0.82
+    min_cluster = 80
 
     cluster_words = list()
 
@@ -48,15 +50,17 @@ def main():
     labels = FastQt(threshold, min_cluster).fit(data, callback_cluster)
     print('Without cluster:', (labels == -1).sum())
     print('Number clusters:', len(cluster_words))
-
-    with open(os.path.join(DATA_DIR, "cluster_words.txt"), 'w+') as f_words:
-        with open(os.path.join(DATA_DIR, "cluster_centroids_words.txt"), 'w+') as f_centroids:
-            f_words.write(str(len(cluster_words)) + '\n')
-            f_centroids.write(str(len(cluster_words)) + ' ' + str(data.shape[1]) + '\n')
-            for i, cluster in enumerate(cluster_words):
-                print(f"{i} = {len(cluster[0])}")
-                f_words.write(" ".join(cluster[0]) + '\n')
-                f_centroids.write(" ".join([str(x) for x in cluster[1]]) + '\n')
+    with open(os.path.join(DATA_DIR, "cluster_names.txt"), 'w+') as f_names:
+        with open(os.path.join(DATA_DIR, "cluster_words.txt"), 'w+') as f_words:
+            with open(os.path.join(DATA_DIR, "cluster_centroids_words.txt"), 'w+') as f_centroids:
+                f_names.write(str(len(cluster_words)) + '\n')
+                f_words.write(str(len(cluster_words)) + '\n')
+                f_centroids.write(str(len(cluster_words)) + ' ' + str(data.shape[1]) + '\n')
+                for i, cluster in enumerate(cluster_words):
+                    print(f"{i} = {len(cluster[0])}")
+                    f_names.write(cluster[0][0] + "|" + str(len(cluster[0])) + '\n')
+                    f_words.write(" ".join(cluster[0]) + '\n')
+                    f_centroids.write(" ".join([str(x) for x in cluster[1]]) + '\n')
 
 
 if __name__ == "__main__":
