eb6e60ef75e98e2051634f52130c8cdde55469e7
andrei.tsypilnikov <andrei.tsypilnikov92@gmail.com>
2020-03-01 08:43:03 +0300

    New scripts for embeddings

--- scripts/embeddings.py
+++ scripts/embeddings.py
@@ -2,6 +2,8 @@ import io
 import os
 import numpy as np
 from sklearn.preprocessing import normalize
+from itertools import combinations
+from scipy.spatial import distance_matrix
 
 from com.expleague.media_space.topics.fast_qt import FastQt
 
@@ -30,16 +32,7 @@ def load_fasttext(fname, skip=0, limit=10e9):
 
 
 # noinspection PyArgumentList
-def main():
-    data, words = load_fasttext(os.path.join(DATA_DIR, "news_dragnet.vec"), 2000, 90000)
-    # data, words = load_glove('lenta-decomp-new', 20000)
-
-    data = normalize(data)
-    #threshold = 0.8
-    #min_cluster = 50
-    threshold = 0.85
-    min_cluster = 100
-
+def calculate_clustering(data, words, threshold=0.8, min_cluster=100):
     cluster_words = list()
 
     def callback_cluster(indices, dist):
@@ -48,20 +41,42 @@ def main():
 
     # labels = FastQt(threshold, min_cluster).fit(data, lambda indices, dist: print(words[indices][:10000]))
     labels = FastQt(threshold, min_cluster).fit(data, callback_cluster)
-    print('Without cluster:', (labels == -1).sum())
-    print('Number clusters:', len(cluster_words))
-    with open(os.path.join(DATA_DIR, "cluster_names.txt"), 'w+') as f_names:
-        with open(os.path.join(DATA_DIR, "cluster_words.txt"), 'w+') as f_words:
-            with open(os.path.join(DATA_DIR, "cluster_centroids_words.txt"), 'w+') as f_centroids:
+    without_clusters = (labels == -1).sum()
+    # print('Without cluster:', without_cluster_num)
+    # print('Number clusters:', len(cluster_words))
+    centroids = list()
+    for i, cluster in enumerate(cluster_words):
+        centroids.append(cluster[1])
+    dist_matrix = distance_matrix(centroids, centroids)
+    inverse_dist_sum = 0
+    for i, j in combinations(range(len(cluster_words)), 2):
+        inverse_dist_sum += 1 / dist_matrix[i][j]
+    return inverse_dist_sum, without_clusters, cluster_words
+
+
+def save_clustering(cluster_words, data, threshold, min_cluster):
+    cluster_names_file = f"cluster_names_{'{:.2f}'.format(threshold)}_{min_cluster}.txt"
+    cluster_words_file = f"cluster_words_{'{:.2f}'.format(threshold)}_{min_cluster}.txt"
+    cluster_centroids_file = f"cluster_centroids_{'{:.2f}'.format(threshold)}_{min_cluster}.txt"
+    with open(os.path.join(DATA_DIR, cluster_names_file), 'w+') as f_names:
+        with open(os.path.join(DATA_DIR, cluster_words_file), 'w+') as f_words:
+            with open(os.path.join(DATA_DIR, cluster_centroids_file), 'w+') as f_centroids:
                 f_names.write(str(len(cluster_words)) + '\n')
                 f_words.write(str(len(cluster_words)) + '\n')
                 f_centroids.write(str(len(cluster_words)) + ' ' + str(data.shape[1]) + '\n')
                 for i, cluster in enumerate(cluster_words):
-                    print(f"{i} = {len(cluster[0])}")
                     f_names.write(cluster[0][0] + "|" + str(len(cluster[0])) + '\n')
                     f_words.write(" ".join(cluster[0]) + '\n')
                     f_centroids.write(" ".join([str(x) for x in cluster[1]]) + '\n')
 
 
 if __name__ == "__main__":
-    main()
+    data, words = load_fasttext(os.path.join(DATA_DIR, "news_dragnet.vec"), 200, 100000)
+    data = normalize(data)
+    min_cluster = 50
+    for th in range(58, 64, 2):
+        threshold = th / 100.0
+        inverse_dist, without_cluster_num, cluster_words = \
+            calculate_clustering(data, words, threshold=threshold, min_cluster=min_cluster)
+        save_clustering(cluster_words, data, threshold, min_cluster)
+        print(threshold, inverse_dist, without_cluster_num, len(cluster_words))
--- scripts/remove_ignore.py
+++ scripts/remove_ignore.py
@@ -4,21 +4,22 @@ import os
 import requests
 import re
 
-DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data")
+DATA_DIR = "/home/tsypia/git/topics/topic_modeling/models/gasparetti"
+
 
 if __name__ == "__main__":
 
-    with open(os.path.join(DATA_DIR, 'models', 'cluster_centroids.txt'), 'r') as f:
+    with open(os.path.join(DATA_DIR, 'cluster_centroids.txt'), 'r') as f:
         num_centroids = f.readline()
         cluster_centroids = f.read().splitlines()
 
-    with open(os.path.join(DATA_DIR, 'models', 'cluster_names.txt'), 'r') as f:
+    with open(os.path.join(DATA_DIR, 'cluster_names.txt'), 'r') as f:
         num_names = f.readline()
         cluster_names = f.read().splitlines()
     assert len(cluster_centroids) == len(cluster_names)
     nums = len([x for x in cluster_names if x != '-ignore-'])
-    with open(os.path.join(DATA_DIR, 'models', 'cluster_names_filtered.txt'), 'w+') as f_n:
-        with open(os.path.join(DATA_DIR, 'models', 'cluster_centroids_filtered.txt'), 'w+') as f_c:
+    with open(os.path.join(DATA_DIR, 'cluster_names_filtered.txt'), 'w+') as f_n:
+        with open(os.path.join(DATA_DIR, 'cluster_centroids_filtered.txt'), 'w+') as f_c:
             f_n.write(str(nums) + '\n')
             f_c.write(str(nums) + ' 100\n')
             for i in range(len(cluster_names)):
